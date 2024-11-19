3c7ee508a8f2f00c86784c43b448ec31896e011e
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-11-18 19:04:05 -0500

    Add swagger ui to render openapi spec

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,17 +3,27 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { authRouter } from './auth/routes.js';
 import { isAuthenticated } from './auth/middleware.js';
+import swaggerUi from 'swagger-ui-express';
+//import swaggerDocument from '../../openapi.json';
+import path from 'path';
+import fs from 'fs';
 
 const app = express();
 
 app.use(cors());
 app.use(cookieParser());
 
+app.use('/api-docs', swaggerUi.serve,
+	swaggerUi.setup(
+		// @ts-ignore
+		JSON.parse(fs.readFileSync(path.resolve(process.cwd(), './openapi.json')))
+	)
+);
 app.get('/', isAuthenticated, (_, res) => {
 	res.send('yerr');
 });
 
-app.use('/auth', authRouter);
+app.use('/v1/auth', authRouter);
 
 const PORT = 4000;
 
